22e9bd325b125df1dfcfc8723f916a5871f47022
João Paulo <104577254+ijoaobarros@users.noreply.github.com>
2025-04-10 13:59:33 -0300

    Inicio

---
+++ ProjetoFinal/MATLAB/PF01_plot_dos_dados_sem_ajustes.m
@@ -0,0 +1,21 @@
+clear; clc;
+
+% Apenas para o plot dos dados
+angulo = load('angulo.mat').Angulo;
+tempo = load('tempo.mat').Tempo;
+figure(1);
+plot(tempo, angulo, 'LineWidth', 1.5, 'Color', 'Blue');
+grid on;
+title('Dados Experimentais')
+xlabel('Tempo [s]');
+ylabel('Ângulo [°]');
+
+% Ajuste de ângulo
+figure(2);
+tempo = tempo(1:1114);
+angulo = [angulo(1:657); angulo(658:929) + 360; angulo(930:1114) + 2*360];
+plot(tempo, angulo, 'LineWidth', 1.5, 'Color', 'Blue');
+grid on;
+title('Dados Experimentais com Ajuste de Posição Ângular (3 Primeiras Voltas)')
+xlabel('Tempo [s]');
+ylabel('Ângulo [°]');
---
+++ ProjetoFinal/MATLAB/PF02_codigo_ajuste_de_parametros.m
@@ -0,0 +1,30 @@
+clc; clear;
+
+% Carregamento dos dados de medição
+angulo = load('angulo.mat').Angulo;
+tempo = load('tempo.mat').Tempo;
+
+% Ajustando os dados de posição angular
+angulo = [angulo(1:657); angulo(658:929) + 360; angulo(930:1114) + 2*360];
+tempo = tempo(1:1114);
+
+% Função de transferência da planta (CubeSat)
+s = tf('s');
+%J = 1    % Momento de inércia inicial
+J = 1000/14585;   % Momento de inércia CubeSat
+G = 1/( J * (s^2) );
+
+tempo_auxiliar = 0:10^(-3):tempo(end);
+modelo = step(G, tempo_auxiliar);
+
+% Plotando gráficos
+figure(1);
+plot(tempo, angulo, 'LineWidth', 1.5, 'Color', 'Blue', 'DisplayName','Saída Experimental'); 
+hold on; grid on;
+plot(tempo_auxiliar, modelo, 'LineWidth', 1.5, 'Color', 'Red');
+title('Saída Experimental x Modelo')
+xlabel('Tempo [s]');
+ylabel('Ângulo [°]');
+legend('Experimental', 'Modelo');
+xlim([0 tempo(end)]);
+hold off;
