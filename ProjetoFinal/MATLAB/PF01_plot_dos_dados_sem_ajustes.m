4bfb231eca06adfcbd2ec8c12f65d0f5765a198d
João Paulo <joaopaulo_us@hotmail.com>
2025-04-11 01:02:13 -0300

    atualização

--- ProjetoFinal/MATLAB/PF01_plot_dos_dados_sem_ajustes.m
+++ ProjetoFinal/MATLAB/PF01_plot_dos_dados_sem_ajustes.m
@@ -4,17 +4,17 @@ clear; clc;
 angulo = load('angulo.mat').Angulo;
 tempo = load('tempo.mat').Tempo;
 figure(1);
-plot(tempo, angulo, 'LineWidth', 1.5, 'Color', 'Blue');
+plot(tempo, angulo, 'LineWidth', 1.5, 'Color', '#C00000');
 grid on;
 title('Dados Experimentais')
 xlabel('Tempo [s]');
 ylabel('Ângulo [°]');
 
-% Ajuste de ângulo
+% Ajuste do ângulo
 figure(2);
 tempo = tempo(1:1114);
 angulo = [angulo(1:657); angulo(658:929) + 360; angulo(930:1114) + 2*360];
-plot(tempo, angulo, 'LineWidth', 1.5, 'Color', 'Blue');
+plot(tempo, angulo, 'LineWidth', 1.5, 'Color', '#C00000');
 grid on;
 title('Dados Experimentais com Ajuste de Posição Ângular (3 Primeiras Voltas)')
 xlabel('Tempo [s]');
